a8937cf945ca9273a9379333498e2891adb26141
Konstantin Nazarov <mail@knazarov.com>
2021-03-15 22:21:54 +0300

    Satisfy the linter

--- Formula/libangle.rb
+++ Formula/libangle.rb
@@ -1,7 +1,7 @@
 class Libangle < Formula
-  desc "A conformant OpenGL ES implementation for Windows, Mac, Linux, iOS and Android."
+  desc "Conformant OpenGL ES implementation for Windows, Mac, Linux, iOS and Android"
   homepage "https://github.com/google/angle"
-  url "git@github.com:google/angle.git", :using => :git, :revision => "a11d65a172f885042cf4fdab5bfd124d174f5190"
+  url "git@github.com:google/angle.git", using: :git, revision: "a11d65a172f885042cf4fdab5bfd124d174f5190"
   version "20210315.1"
   license "BSD-3-Clause"
 
@@ -9,20 +9,19 @@ class Libangle < Formula
   depends_on "ninja" => :build
 
   resource "depot_tools" do
-    url "https://chromium.googlesource.com/chromium/tools/depot_tools.git", :revision => "8e2667e04d9282b6cb24e1086a246247036393c5"
+    url "https://chromium.googlesource.com/chromium/tools/depot_tools.git", revision: "8e2667e04d9282b6cb24e1086a246247036393c5"
   end
-
 
   def install
     mkdir "build" do
-      resource("depot_tools").stage {
+      resource("depot_tools").stage do
         path = PATH.new(ENV["PATH"], Dir.pwd)
         with_env(PATH: path) do
           Dir.chdir(buildpath)
 
           system "python2", "scripts/bootstrap.py"
           system "gclient", "sync"
-          system "gn", 'gen', '--args=use_custom_libcxx=false', './angle_build'
+          system "gn", "gen", "--args=use_custom_libcxx=false", "./angle_build"
           system "ninja", "-C", "angle_build"
           lib.install "angle_build/libabsl.dylib"
           lib.install "angle_build/libEGL.dylib"
@@ -30,7 +29,7 @@ class Libangle < Formula
           lib.install "angle_build/libchrome_zlib.dylib"
           include.install Pathname.glob("include/*")
         end
-      }
+      end
     end
   end
 
--- Formula/virglrenderer.rb
+++ Formula/virglrenderer.rb
@@ -1,44 +1,32 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Virglrenderer < Formula
   desc "VirGL virtual OpenGL renderer"
   homepage "https://gitlab.freedesktop.org/virgl/virglrenderer"
-  url "https://github.com/freedesktop/virglrenderer.git", :using => :git, :revision => "d470a2df588d86fca460db889bfe5b2cce7caebb"
-  license "MIT"
+  url "https://github.com/freedesktop/virglrenderer.git", using: :git, revision: "d470a2df588d86fca460db889bfe5b2cce7caebb"
   version "20210315.1"
+  license "MIT"
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "knazarov/qemu-virgl/libepoxy-angle"
   depends_on "knazarov/qemu-virgl/libangle"
+  depends_on "knazarov/qemu-virgl/libepoxy-angle"
 
   # waiting for upstreaming of https://github.com/akihikodaki/virglrenderer/tree/macos
   patch :p1 do
-    url "https://raw.githubusercontent.com/knazarov/homebrew-qemu-virgl/master/Patches/virglrenderer-v01.diff"
+    url "https://raw.githubusercontent.com/knazarov/homebrew-qemu-virgl/e6d2da200586ab412791d80ef38abbeb335afa3d/Patches/virglrenderer-v01.diff"
     sha256 "44d745753459315733e4a2fff2e6bd731a85cc9250ac89c734f8a81dda17ce09"
   end
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     mkdir "build" do
-      system "meson", *std_meson_args, "-Dc_args=-I#{Formula["libepoxy-angle"].opt_prefix}/include", "-Dc_link_args=-L#{Formula['libepoxy-angle'].opt_prefix}/lib", ".."
+      system "meson", *std_meson_args, "-Dc_args=-I#{Formula["libepoxy-angle"].opt_prefix}/include",
+             "-Dc_link_args=-L#{Formula["libepoxy-angle"].opt_prefix}/lib", ".."
       system "ninja", "-v"
       system "ninja", "install", "-v"
     end
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test virglrenderer`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "true"
   end
 end
