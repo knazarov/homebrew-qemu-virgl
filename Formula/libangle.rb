da3599088f1c79be1a9b9f64ba1c97eb27ce9945
Konstantin Nazarov <mail@knazarov.com>
2021-03-16 16:54:39 +0300

    Revert "Fix linter errors and implement bottle packing with gitlab CI"

--- Formula/libangle.rb
+++ Formula/libangle.rb
@@ -1,27 +1,28 @@
 class Libangle < Formula
-  desc "Conformant OpenGL ES implementation for Windows, Mac, Linux, iOS and Android"
-  homepage "https://github.com/google/angle"
-  url "https://github.com/google/angle.git", using: :git, revision: "a11d65a172f885042cf4fdab5bfd124d174f5190"
+  desc ""
+  homepage ""
+  url "git@github.com:google/angle.git", :using => :git, :revision => "a11d65a172f885042cf4fdab5bfd124d174f5190"
+  license ""
   version "20210315.1"
-  license "BSD-3-Clause"
 
   depends_on "meson" => :build
   depends_on "ninja" => :build
 
   resource "depot_tools" do
-    url "https://chromium.googlesource.com/chromium/tools/depot_tools.git", revision: "8e2667e04d9282b6cb24e1086a246247036393c5"
+    url "https://chromium.googlesource.com/chromium/tools/depot_tools.git", :using => :git, :tag => "master"
   end
+
 
   def install
     mkdir "build" do
-      resource("depot_tools").stage do
+      resource("depot_tools").stage {
         path = PATH.new(ENV["PATH"], Dir.pwd)
         with_env(PATH: path) do
           Dir.chdir(buildpath)
 
           system "python2", "scripts/bootstrap.py"
           system "gclient", "sync"
-          system "gn", "gen", "--args=use_custom_libcxx=false", "./angle_build"
+          system "gn", 'gen', '--args=use_custom_libcxx=false', './angle_build'
           system "ninja", "-C", "angle_build"
           lib.install "angle_build/libabsl.dylib"
           lib.install "angle_build/libEGL.dylib"
@@ -29,7 +30,7 @@ class Libangle < Formula
           lib.install "angle_build/libchrome_zlib.dylib"
           include.install Pathname.glob("include/*")
         end
-      end
+      }
     end
   end
 
